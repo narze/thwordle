cd232f40c358e74da17fc88106dc63c4deb99abd
Manassarn "Noom" Manoonchai <248741+narze@users.noreply.github.com>
2022-02-10 22:48:10 +0700

    Add special mode (#30)
    * Add special page
    
    * Use hash routings
    
    * Add twitter link
    
    * The word

---
+++ src/Main.svelte
@@ -0,0 +1,12 @@
+<!-- Main.svelte -->
+<script>
+  import url from "./lib/url"
+  import App from "./App.svelte"
+  import Special from "./Special.svelte"
+</script>
+
+{#if $url.hash.indexOf("#/s/") === 0 || $url.hash === "#/"}
+  <Special specialId={$url.hash.split(["#/s/"])[1]} />
+{:else}
+  <App />
+{/if}
