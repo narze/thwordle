2018c928526209d5e050a469d040022c0e066f5a
Manassarn Manoonchai <manassarn@gmail.com>
2022-02-20 02:45:21 +0700

    Mock onMount

--- src/App.test.ts
+++ src/App.test.ts
@@ -4,8 +4,17 @@ import { render } from "@testing-library/svelte"
 
 import App from "./App.svelte"
 
+jest.mock("svelte", () => {
+  const originalModule = jest.requireActual("svelte")
+
+  return {
+    // __esModule: true,
+    ...originalModule,
+    onMount: jest.fn((args) => console.log("onMount called", args)),
+  }
+})
+
 it("works", () => {
   const { getByText } = render(App, {})
-
   expect(getByText("Thwordle")).toBeInTheDocument()
 })
