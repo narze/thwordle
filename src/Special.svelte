10d0704a65285371e71df6116a6e920c3cdc285d
Manassarn Manoonchai <manassarn@gmail.com>
2022-03-13 00:52:16 +0700

    refactor: move special words to another file

--- src/Special.svelte
+++ src/Special.svelte
@@ -19,6 +19,7 @@
   import SettingModal from "./lib/SettingModal.svelte"
   import StatsModal from "./lib/StatsModal.svelte"
   import { layouts } from "./lib/layouts"
+  import specialWords from "./lib/special-words"
 
   export let specialId
 
@@ -37,82 +38,7 @@
 
   const gtagId = "G-F2Q37REQE6"
 
-  const specialEntries = {
-    special1: {
-      day: "S1",
-      word: "ชุดโกโกวา",
-    },
-    s2: {
-      day: "S2",
-      word: "ยาก",
-    },
-    s3b3a35b99: {
-      day: "S3",
-      word: "ขะมักเขม้น",
-    },
-    s4_7d7f803a: {
-      day: "S4",
-      word: "โฆษณา",
-    },
-    s5_34c6e86d: {
-      day: "S5",
-      word: "แพนด้า",
-    },
-    s6_dfbe2baa: {
-      day: "S6",
-      word: "ปาเลสไตน์",
-    },
-    s7_761d3701: {
-      day: "S7",
-      word: "บางกอก",
-    },
-    s8_cf3db809: {
-      day: "S8",
-      word: "สั้น",
-    },
-    s9_03db: {
-      day: "S9",
-      word: "มนุษย์",
-    },
-    s10_4aa3: {
-      day: "S10",
-      word: "เยอรมัน",
-    },
-    s11_e846: {
-      day: "S11",
-      word: "ประสิทธิภาพ",
-    },
-    s12_9c55: {
-      day: "S12",
-      word: "ศฤงคาร",
-    },
-    s13_68a6: {
-      day: "S13",
-      word: "องคาพยพ",
-    },
-    s14_193f: {
-      day: "S14",
-      word: "มิจฉาทิฐิ",
-    },
-    s15_472e: {
-      day: "S15",
-      word: "คว่ำบาตร",
-    },
-    s16_e481: {
-      day: "S16",
-      word: "หงสาวดี",
-    },
-    s17_2f5a: {
-      day: "S17",
-      word: "พจนานุกรม",
-    },
-    s18_03fe: {
-      day: "S18",
-      word: "กฤษฎีกา",
-    },
-  }
-
-  if (!specialEntries[specialId]) {
+  if (!specialWords[specialId]) {
     alert("รหัสลับผิด กรุณาลองใหม่อีกครั้ง")
     window.location.href = "/"
   }
@@ -120,12 +46,12 @@
   $: rows = layouts[$settings.layout].rows
   $: rowsShifted = layouts[$settings.layout].rowsShifted
 
-  const specialDay = specialEntries[specialId]?.day
+  const specialDay = specialWords[specialId]?.day
 
   const attemptLimit = 6
 
   let input = ""
-  $: solution = specialEntries[specialId]?.word
+  $: solution = specialWords[specialId]?.word
   let attempts: string[] = $data[specialDay]?.attempts || []
   $: validations = attempts.map((word) => validateWord(word, solution))
   let gameEnded = !!$data[specialDay]?.win || !!$data[specialDay]?.lose
