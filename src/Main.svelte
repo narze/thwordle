9d34b834a8524c81cf93d51a8ee7094963723bcd
Manassarn Manoonchai <manassarn@gmail.com>
2022-05-11 22:24:08 +0700

    Add training mode

--- src/Main.svelte
+++ src/Main.svelte
@@ -3,10 +3,13 @@
   import url from "./lib/url"
   import App from "./App.svelte"
   import Special from "./Special.svelte"
+  import Training from "./Training.svelte"
 </script>
 
 {#if $url.hash.indexOf("#/s/") === 0 || $url.hash === "#/"}
   <Special specialId={$url.hash.split(["#/s/"])[1]} />
+{:else if $url.hash.indexOf("#/training") === 0}
+  <Training />
 {:else}
   <App />
 {/if}
