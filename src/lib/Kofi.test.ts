12587f9acd7d912875bbe1cd0a57b5b268e32b9a
Manassarn Manoonchai <manassarn@gmail.com>
2025-10-03 22:46:48 +0700

    Replace jest with vitest

--- src/lib/Kofi.test.ts
+++ src/lib/Kofi.test.ts
@@ -1,10 +1,9 @@
-import "@testing-library/jest-dom"
-
+import { test } from "vitest"
 import { render } from "@testing-library/svelte"
 
 import Kofi from "./Kofi.svelte"
 
-it("works", () => {
+test("works", () => {
   const { getByText } = render(Kofi, { name: "foo", label: "Pay me" })
 
   expect(getByText("Pay me")).toBeInTheDocument()
