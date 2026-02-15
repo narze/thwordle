5e00ed227d1c10c0132ef78f9afc66888c604ed6
Manassarn Manoonchai <manassarn@gmail.com>
2026-02-15 16:19:58 +0700

    Fix navbar mobile menu

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -136,7 +136,6 @@
                   on:click={() => {
                     goto("/")
                   }}>Daily</button>
-                >
               {/if}
               {#if daily || special}
                 <button
@@ -144,7 +143,6 @@
                   on:click={() => {
                     goto("/unlimited")
                   }}>Unlimited</button>
-                >
               {/if}
               {#if daily || training}
                 <button
