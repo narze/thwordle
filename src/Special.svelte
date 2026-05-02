9c7532c5da9eeed642acb745303464d805e688a9
Manassarn Manoonchai <manassarn@gmail.com>
2026-05-02 10:34:42 +0800

    Fix spin animation

--- src/Special.svelte
+++ src/Special.svelte
@@ -327,7 +327,7 @@
               colors[correct] || "bg-white"
             } attempt-key border-solid border-2 flex items-center justify-center mx-0.5 text-3xl font-bold text-white
       rounded`}
-            in:spinAnimation={{ duration: 500, delay: 150 * idx }}
+            in:spinAnimation|global={{ duration: 500, delay: 150 * idx }}
           >
             {char ?? ""}
           </div>
