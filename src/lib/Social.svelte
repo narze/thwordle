8097325c2e316504ed17769073aae886136cdb45
Manassarn Manoonchai <manassarn@gmail.com>
2026-05-02 09:17:58 +0700

    Minimal upgrade

--- src/lib/Social.svelte
+++ src/lib/Social.svelte
@@ -1,13 +1,30 @@
 <script lang="ts">
-  import { Facebook, Twitter } from "svelte-share-buttons-component"
-
   export let url: string
   export let title: string
+
+  $: facebookHref = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(url)}`
+  $: twitterHref = `https://twitter.com/intent/tweet?text=${encodeURIComponent(title)}&url=${encodeURIComponent(url)}`
 </script>
 
 {#if url}
   <div class="flex gap-2 justify-center w-full fixed bottom-4 center">
-    <Facebook class="h-10 w-10 rounded" {url} />
-    <Twitter class="h-10 w-10 rounded" text={title} {url} />
+    <a
+      class="h-10 w-10 rounded flex items-center justify-center bg-[#1877F2] text-white font-bold"
+      href={facebookHref}
+      target="_blank"
+      rel="noopener noreferrer"
+      aria-label="Share on Facebook"
+    >
+      f
+    </a>
+    <a
+      class="h-10 w-10 rounded flex items-center justify-center bg-black text-white font-bold"
+      href={twitterHref}
+      target="_blank"
+      rel="noopener noreferrer"
+      aria-label="Share on Twitter"
+    >
+      X
+    </a>
   </div>
 {/if}
--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,5 @@
-import sveltePreprocess from "svelte-preprocess"
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte"
 
 export default {
-  // Consult https://github.com/sveltejs/svelte-preprocess
-  // for more information about preprocessors
-  preprocess: [
-    sveltePreprocess({
-      postcss: true,
-    }),
-  ],
+  preprocess: [vitePreprocess()],
 }
